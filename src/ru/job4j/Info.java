9b2da4f246bf79515971e4f6fdc511be249b8576
Mazay98 <zavolod@yandex.ru>
2020-10-12 00:58:53 +0300

    3.4. Заголовок в файле java [#385090]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+import java.util.Date;
+
+public class Info {
+    public static void main(String [] args){
+        Date date = new Date();
+        System.out.println(date.toString());
+    }
+}
