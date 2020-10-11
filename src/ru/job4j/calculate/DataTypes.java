9e14ae83cef4936c6125e908916e753e9d59ce3e
Mazay98 <zavolod@yandex.ru>
2020-10-12 01:23:33 +0300

    1. Определите тип данных для переменных. [#385073]

---
+++ src/ru/job4j/calculate/DataTypes.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculate;
+
+public class DataTypes {
+    public static void main(String [] args){
+        byte age;
+        char gender;
+        float height;
+        int countBrickForBuildHouse;
+        long countBacterium;
+        double distanceToSpaceObjects;
+        boolean accessToWork;
+        long fileSize;
+    }
+}
+
+//- размер файла.
