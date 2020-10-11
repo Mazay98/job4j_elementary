8dab986a143749e41ff746737c449e91fb3b457b
Mazay98 <zavolod@yandex.ru>
2020-10-12 01:29:10 +0300

    2. Переменные. [#385074]

--- src/ru/job4j/calculate/DataTypes.java
+++ src/ru/job4j/calculate/DataTypes.java
@@ -2,15 +2,13 @@ package ru.job4j.calculate;
 
 public class DataTypes {
     public static void main(String [] args){
-        byte age;
-        char gender;
-        float height;
-        int countBrickForBuildHouse;
-        long countBacterium;
-        double distanceToSpaceObjects;
-        boolean accessToWork;
-        long fileSize;
+        byte age = 21;
+        char gender = 'M';
+        float height = (float) 184.3;
+        int countBrickForBuildHouse = 18356252;
+        long countBacterium = 852642523;
+        double distanceToSpaceObjects = 3658451512213.364151515;
+        boolean accessToWork = true;
+        long fileSize = 265325254;
     }
 }
-
-//- размер файла.
