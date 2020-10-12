1e4545e3b3b284d1fa29f785e6301dc34d1b38cc
Mazay98 <zavolod@yandex.ru>
2020-10-12 18:57:29 +0300

    5. Конвертер валюты [#385068]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,15 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToCourse(int value, float course) {
+        return value / course;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToCourse(140, 70);
+        float dollar = Converter.rubleToCourse(140, 60);
+        System.out.println("140 rubles are " + String.format("%.2f",euro) + " euro.");
+        System.out.println("140 rubles are " + String.format("%.2f",dollar) + " dollar.");
+    }
+}
